87dac6ca619cf3ffba61ddd19430896146d2e71f
Nnabueze Uhiara <nuhiara@gmail.com>
2024-04-09 13:09:36 +0100

    Whitelist contract for NFT project, CryptoDevs completed and deployed to sepolia

--- src/Whitelist.sol
+++ src/Whitelist.sol
@@ -1,28 +1,14 @@
 // SPDX-License-Identifier: MIT 
-pragma solidity ^0.8.24;
+pragma solidity ^0.8.25;
 
 contract Whitelist {
     uint8 public maxWhitelistCount;
     uint8 whitelistCount;
-    address ownerAddress;
-
-    modifier onlyOwner {
-        require(msg.sender == ownerAddress, "Unauthorized!");
-        _;
-    }
 
     mapping(address => bool) public whitelistedAddresses;
 
     constructor(uint8 maxWhitelist) {
         maxWhitelistCount = maxWhitelist;
-        ownerAddress = msg.sender;
-    }
-
-    function whitelistUser(address _user) public onlyOwner {
-        require(!whitelistedAddresses[_user], "User has already been whitelisted!");
-        require(whitelistCount + 1 <= maxWhitelistCount, "Sorry, Whitelist limit exceeded!");
-        whitelistedAddresses[_user] = true;
-        whitelistCount += 1;
     }
 
     function addToWhitelist() public {
@@ -30,21 +16,6 @@ contract Whitelist {
         require(whitelistCount + 1 <= maxWhitelistCount, "Sorry, Whitelist limit exceeded!");
         whitelistedAddresses[msg.sender] = true;
         whitelistCount += 1;
-    }
-
-    function revokeWhitelist(address _user) public onlyOwner {
-        require(whitelistedAddresses[_user], "User is not whitelisted");
-        whitelistedAddresses[_user] = false;
-        whitelistCount -= 1;
-    }
-
-    function checkWhitelistStatus() public view returns (bool) {
-        return whitelistedAddresses[msg.sender];
-    }
-
-    function increaseWhitelistMax(uint8 newLimit) public onlyOwner {
-        require(newLimit > maxWhitelistCount, "New limit has to be greater than the current max whitelist count");
-        maxWhitelistCount = newLimit;
     }
 }
 
